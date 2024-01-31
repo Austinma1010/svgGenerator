f7d92fd03b2fd0350f6ac79a3148f5df791d4666
Austinma10 <austinma2168@gmail.com>
2024-01-31 17:31:02 -0500

    Created all needed files

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const createSvg = require('./lib/createSvg');
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter text:',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'Enter color:',
+        name: 'color',
+    },
+    {
+        type: 'Checkbox',
+        message: 'Select shape',
+        name: 'shape',
+        choices: [
+            {name: 'circle'},
+            {name: 'triangle'},
+            {name: 'square'},
+        ],
+    }
+];
+
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        writeFile(response);
+    });
+}
+
+function writeFile(data) {
+    const svgFile = createSvg(data);
+
+    fs.writeFile(data.text + '.svg', svgFile, (err) =>
+    err ? console.error(err) : console.log('Success!'));
+
+}
+init();
